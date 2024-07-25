ae2fc48ab3c8bd4971d1392d30438ca556fddc04
Sai santosh <me.sai.santosh@gmail.com>
2024-07-25 20:52:26 +0530

    Signal Level math explained

--- src/widgets/signal-level.tsx
+++ src/widgets/signal-level.tsx
@@ -40,18 +40,33 @@ export const SignalLevel = ({
 }: SignalLevelProps) => {
   const [level, setLevel] = useState<number>(min);
 
+  //In Percentage
   const minWidth = 10;
 
+  /*
+    In Percentage
+    Maximum amount of increment the bar can take
+    This gives us the increment for each bar,
+    so that width increment is evenly distributed until 100%.
+    This will be mitigated for small number of levels in the next step.
+  */
   const maxIncrement = (100 - minWidth) / levels;
 
+  // All Calculations are done in reverse order for the ease of understanding
   const bars = useMemo(
     () =>
       new Array(levels)
         .fill(0)
         .map((_, index) =>
-          index < 1
-            ? minWidth
-            : Math.min(maxIncrement, minWidth) * index + minWidth
+          index === 0
+            ? minWidth // First bar takes the fixed smallest width
+            : /*
+                For smaller level count where bars doesn't have to
+                occupy 100%, maxIncrement will be greater than minWidth.
+                We will favour the minWidth here,
+                so that even distribution happens among all bars.
+              */
+              minWidth + Math.min(maxIncrement, minWidth) * index
         )
         .reverse(),
     [levels, maxIncrement]
