2880a44274e841d74888db61551e7e1b9ffaf0d7
Sai santosh <me.sai.santosh@gmail.com>
2024-07-27 00:27:54 +0530

    Vitest setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
+/// <reference types="vitest" />
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    TanStackRouterVite({
+      routesDirectory: path.resolve(__dirname, "src/routes"),
+      generatedRouteTree: path.resolve(__dirname, "src/routeTree.gen.ts"),
+    }),
     react({
       jsxImportSource: "@emotion/react",
       babel: {
@@ -15,6 +21,15 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  test: {
+    globals: true,
+    environment: "happy-dom",
+    coverage: {
+      reportsDirectory: "./coverage",
+      provider: "v8",
+      reporter: ["json", "html", "html-spa"],
     },
   },
 });
